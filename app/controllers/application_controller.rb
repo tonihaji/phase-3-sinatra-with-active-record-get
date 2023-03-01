2a3d7ebf6c3ffe2ed6833e1e0fc6890a8ebf2f56
ehachi <ehajianto@gmail.com>
2023-03-01 10:55:58 +0300

    ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
-  get '/' do
-    { message: "Hello world" }.to_json
+  get '/games' do
+    games = Game.all
+    games.to_json(include: { reviews: { include: :user } })
   end
 
+  get '/games/:id' do
+    game = Game.find(params[:id])
+    game.to_json(include: { reviews: { include: :user } })
+  end
 end
